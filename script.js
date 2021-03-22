5275f2cc13a236c484fa8fd285b1fb3630cd40fd
Lucre <josielindamaldonado@gmail.com>
2021-03-22 18:49:57 -0400

    Began java and added HW instructions to readme.

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-console.log("Hello World")
+var questions = [
+  {
+    question: "What is the meaning o",
+    answers: [
+      { a: "answer", correct: true },
+      { b: "answer", correct: false },
+    ],
+  },
+  {},
+  {},
+];
+//DOM ELEMENTS
+var startButton = document.getElementById("start-button");
+
+startButton.addEventListener("click", startGame);
+
+function startGame() {
+  console.log("start the game");
+  //start timer
+  // startTimer()
+  // hide the instuctions
+  //hideInstructions()
+  //show the first questions
+}
