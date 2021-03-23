9aca59e55bdc0a47c6dfc562a977c4a6c031097c
Lucre <josielindamaldonado@gmail.com>
2021-03-23 09:13:55 -0400

    began adding questions to JS and working on html and css.

--- script.js
+++ script.js
@@ -1,21 +1,42 @@
 var questions = [
   {
-    question: "What is the meaning o",
+    question: "Commonly used data types DO NOT include: ",
     answers: [
-      { a: "answer", correct: true },
-      { b: "answer", correct: false },
+      { a: "alerts", correct: true },
+      { b: "booleans", correct: false },
+      { c: "numbers", correct: false },
+      { d: "strings", correct: false },
     ],
+  }
+  
+  {
+      question: "The condition in an if / else statement is enclosed within _______.",
+      answers: [
+          { a: "parantheses", correct: false },
+          { b: "qutoes", correct: false },
+          { c: "curly brackets", correct: true },
+          { d: "square brackets", correct: false },
+      ],
   },
-  {},
-  {},
+
+  {
+      question: "Arrays in Javascript can be used to store ___________.",
+      answers: [ 
+          { a: "other arrays", correct: false },
+          { b: "numbers and strings", correct: false },
+          { c: "booleans", correct: false},
+          { d: "all of the above", correct: true },
+
+      ],
+  },
 ];
 //DOM ELEMENTS
-var startButton = document.getElementById("start-button");
+// var startButton = document.getElementById("start-button");
 
-startButton.addEventListener("click", startGame);
+// startButton.addEventListener("click", startGame);
 
-function startGame() {
-  console.log("start the game");
+// function startGame() {
+//   console.log("start the game");
   //start timer
   // startTimer()
   // hide the instuctions
